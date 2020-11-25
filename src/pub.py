6102a19534b836d0f039a4208815f3a053a4f54b
Simon Charters <simon259@hotmail.co.uk>
2020-11-25 11:41:48 +0000

    drink and pub initialised

--- src/pub.py
+++ src/pub.py
@@ -1,7 +1,14 @@
+from src.drink import Drink
+
 class Pub:
 
     def __init__(self, name, till):
         self.name = name
         self.till = till
+        beer = Drink("Beer", 3.00)
+        wine = Drink("Wine", 8.00)
+        whiskey = Drink("Whiskey", 4.50)
+        vodka = Drink("Vodka", 1.00)
+        self.drinks = [beer, wine, whiskey, vodka]
 
     
--- tests/pub_test.py
+++ tests/pub_test.py
@@ -6,6 +6,13 @@ class TestPub(unittest.TestCase):
         self.pub = Pub("The Prancing Pony", 100.00)
 
     def test_pub_has_name(self):
-        self.assertEquals("The Prancing Pony", self.pub.name)
+        self.assertEqual("The Prancing Pony", self.pub.name)
+
+    def test_pub_till(self):
+        self.assertEqual(100.00, self.pub.till)
+        
+    
+    def test_pub_has_drinks(self):
+        self.assertEqual(4, len(self.pub.drinks))
 
     
